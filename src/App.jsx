2c01ac1c6c5b1250bf85b1d607b8a1ee22338466
Luke Davies <luke.davies@junitech.com>
2025-01-23 16:03:25 +0000

    create form component and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
-// import Form from "./components/Form/Form.jsx";
+import Form from "./components/Form/Form.jsx";
 import Flashcards from "./components/Flashcards/Flashcards.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   const [cardList, setCardList] = useState([
-    { question: "What is React?",
-      answer: "React is a JavaScript library aiming to building UIs based on components more seamless." },
-    { question: "What is JSX?",
-      answer: "JSX stands for JavaScript XML. It allows us to write HTML in React." },
-    { question: "How do you create a React component?",
-      answer: "Components can be created as functions or classes that return JSX" },
+    {
+      question: "What is React?",
+      answer:
+        "React is a JavaScript library aiming to building UIs based on components more seamless.",
+    },
+    {
+      question: "What is JSX?",
+      answer:
+        "JSX stands for JavaScript XML. It allows us to write HTML in React.",
+    },
+    {
+      question: "How do you create a React component?",
+      answer:
+        "Components can be created as functions or classes that return JSX",
+    },
   ]);
+
+  function handleCardAddClick(e) {
+    e.preventDefault();
+    const questionValue = e.target.question.value;
+    const answerValue = e.target.answer.value;
+    const newCardList = [...cardList];
+    newCardList.push({
+      question: questionValue,
+      answer: answerValue,
+    });
+    setCardList(newCardList);
+  }
 
   return (
     <>
       <Header />
-      {/* <Form /> */}
+      <Form handleCardAddClick={handleCardAddClick} />
       <Flashcards cardList={cardList} />
       <Footer />
     </>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,3 +1,21 @@
-function Form() {}
+function Form({ handleCardAddClick }) {
+  return (
+    <form method="post" onSubmit={(e) => handleCardAddClick(e)}>
+      <label htmlFor="question">Question</label>
+      <input
+        placeholder="write something here"
+        name="question"
+        id="question"
+      ></input>
+      <label htmlFor="answer">Answer</label>
+      <input
+        placeholder="write something here"
+        name="answer"
+        id="answer"
+      ></input>
+      <button type="submit">Add</button>
+    </form>
+  );
+}
 
 export default Form;
