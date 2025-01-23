72780f989e84fc98e71ebef5ca5814139a964c99
Luke Davies <luke.davies@junitech.com>
2025-01-23 13:39:06 +0000

    full component files created

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header.jsx";
+import Form from "./components/Form/Form.jsx";
+import Flashcards from "./components/Flashcards/Flashcards.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
-  const [cardList, setCardList] = useState([ null, null, null ]);
-  
+  const [cardList, setCardList] = useState([
+    { question: "question 1", answer: "answer 1" },
+    { question: "question 2", answer: "answer 2" },
+    { question: "question 3", answer: "answer 3" },
+  ]);
+
   return (
     <>
       <Header />
       <Form />
       <Flashcards />
-      <Footer />      
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
