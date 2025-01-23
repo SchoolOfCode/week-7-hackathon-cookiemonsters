4b8fbb8feb539c86744cf6b76fb8fb01c961d03a
Holly <162161116+Holl4444@users.noreply.github.com>
2025-01-23 16:39:40 +0000

    Initial styling for header and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,21 @@
-import reactLogo from "../../assets/react.svg";
+import reactLogo from '../../assets/react.svg';
+import './Footer.css';
 
 function Footer() {
   return (
-    <div>
-      <p><small>Built with React</small></p>
+    <footer>
+      <p>
+        <small>Built with React</small>
+      </p>
       <a href="https://react.dev" target="_blank">
-        <img id="footer-logo" src={reactLogo} className="logo react" alt="React logo" />
+        <img
+          id="footer-logo"
+          src={reactLogo}
+          className="logo react"
+          alt="React logo"
+        />
       </a>
-    </div>
+    </footer>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
-import reactLogo from "../../assets/react.svg";
+import reactLogo from '../../assets/react.svg';
+import './Header.css';
 
 function Header() {
   return (
-    <div>
+    <header>
       <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
+        <img
+          src={reactLogo}
+          className="logo react"
+          alt="React logo"
+        />
       </a>
-      <h1>React Flashcards</h1>
-      <h3>🧠 Expand Your React Knowledge, One Flashcard at a Time! 🐌</h3>
-    </div>
+      <h2>React Flashcards</h2>
+      <h5>
+        🧠 Expand Your React Knowledge, One Flashcard at a Time! 🐌
+      </h5>
+    </header>
   );
 }
 
