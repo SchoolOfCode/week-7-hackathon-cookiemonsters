3ae848ffa6dd7f95f721db8a7d9531e3192fa3d3
Holly <162161116+Holl4444@users.noreply.github.com>
2025-01-24 10:30:11 +0000

    fix pages update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/week-7-hackathon-cookiemonsters',
+  plugins: [react()],
 });
